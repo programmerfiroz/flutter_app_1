edd358d1c71c1a273f8a1d82e9932fdb446886d2
programmerfiroz <firozm613@gmail.com>
2025-03-24 21:38:52 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App1());
+}
+
+class App1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter App 1')),
+        body: Center(child: Text('Hello from App 1!')),
+      ),
+    );
+  }
+}
